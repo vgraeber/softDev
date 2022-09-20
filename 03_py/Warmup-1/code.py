0c1d13f110a2aa16068818dfc7f06bdd21c2686e
vivian.graeber <73133143+vgraeber@users.noreply.github.com>
2022-09-20 16:03:25 -0400

    Update code.py

--- 03_py/Warmup-1/code.py
+++ 03_py/Warmup-1/code.py
@@ -1,70 +1,70 @@
 def sleep_in(weekday, vacation):
-  if ((vacation == True) or (weekday == False)) :
+  if ((vacation == True) or (weekday == False)):
     return True
   else :
     return False
 
 def monkey_trouble(a_smile, b_smile):
-  if (a_smile == b_smile) :
+  if (a_smile == b_smile):
     return True
   else :
     return False
 
 def sum_double(a, b):
-  if (a != b) :
+  if (a != b):
     return (a + b)
   else :
     return (2*(a + b))
 
-def diff21(n) :
-  if (n > 21) :
+def diff21(n):
+  if (n > 21):
     return (2 * (abs(21 - n)))
-  else :
+  else:
     return (abs(21 - n))
 
 def parrot_trouble(talking, hour):
-  if ((talking == True) and ((hour < 7) or (hour > 20))) :
+  if ((talking == True) and ((hour < 7) or (hour > 20))):
     return True
-  else :
+  else:
     return False
 
 def makes10(a, b):
-  if ((a == 10) or (b == 10)) :
+  if ((a == 10) or (b == 10)):
     return True
-  if ((a + b) == 10) :
+  if ((a + b) == 10):
     return True
-  else :
+  else:
     return False
 
 def near_hundred(n):
-  if ((abs(100 - n) <= 10) or (abs(200 - n) <= 10)) :
+  if ((abs(100 - n) <= 10) or (abs(200 - n) <= 10)):
     return True
-  else :
+  else:
     return False
 
-def pos_neg(a, b, negative) :
-  if (negative) :
+def pos_neg(a, b, negative):
+  if (negative):
     return ((a < 0) and (b < 0))
   return ((a * b) < 0)
 
-def not_string(str) :
-  if (str[0:3] == 'not') :
+def not_string(str):
+  if (str[0:3] == 'not'):
     return str
-  else :
+  else:
     str2 = 'not ' + str
     return (str2)
 
-def missing_char(str, n) :
+def missing_char(str, n):
   str2 = (str[:n] + str[(n + 1):])
   return str2
 
-def front_back(str) :
-  if (len(str) <= 1) :
+def front_back(str):
+  if (len(str) <= 1):
     return str
-  else :
+  else:
     str2 = str[(len(str)-1)] + str[1:(len(str)-1)] + str[0]
     return str2
 
-def front3(str) :
+def front3(str):
   str2 = (str[0:3] * 3)
   return str2
