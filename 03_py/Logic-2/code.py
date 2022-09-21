c4aa0ecd5291c32a63507656ddaad2c97f067bf4
vivian.graeber <73133143+vgraeber@users.noreply.github.com>
2022-09-21 09:22:45 -0400

    Update code.py

--- 03_py/Logic-2/code.py
+++ 03_py/Logic-2/code.py
@@ -1,38 +1,25 @@
 def make_bricks(small, big, goal):
-  if (goal == 0):
-    return True
-  else:
-    if (goal > (small + (big * 5))):
-      return False
-    elif (small < (goal % 5)):
-      return False
-    else:
-      r = goal % 5
-      small -= r
-      goal -= r
-      if (((big * 5) + small) >= goal):
-        return True
-      else:
-        return False
+  if (goal > (small + (big * 5))):
+    return False
+  elif (small < (goal % 5)):
+    return False
+  return True
 
 def lone_sum(a, b, c):
-  a2 = a
-  b2 = b
-  c2 = c
-  if (a == b == c) 
-    a2 = 0
-    b2 = 0
-    c2 = 0
+  if (a == b == c):
+    a = 0
+    b = 0
+    c = 0
   elif (a == b):
-    a2 = 0
-    b2 = 0
-  elif (a == c) 
-    a2 = 0
-    c2 = 0
-  elif (b == c) 
-    b2 = 0
-    c2 = 0
-  return (a2 + b2 + c2)
+    a = 0
+    b = 0
+  elif (a == c):
+    a = 0
+    c = 0
+  elif (b == c):
+    b = 0
+    c = 0
+  return (a + b + c)
 
 def lucky_sum(a, b, c) 
   if (a == 13):
@@ -55,11 +42,7 @@ def no_teen_sum(a, b, c):
   return (fix_teen(a) + fix_teen(b) + fix_teen(c))
 
 def round10(num):
-  if ((num % 10) >= 5):
-    num = num + (10 - (num % 10))
-  else:
-    num = num - (num % 10)
-  return num
+  return int(round(num, -1))
 
 def round_sum(a, b, c):
   return (round10(a) + round10(b) + round10(c))
@@ -70,24 +53,19 @@ def close_far(a, b, c):
       return True
     elif ((abs(c - a) >= 2) and (abs(c - b) >= 2)):
       return True
-    else:
-      return False
-  else:
     return False
+  return False
 
 def make_chocolate(small, big, goal):
-  if ((small + (big * 5)) < goal):
+  if (goal > (small + (big * 5))):
+    return -1
+  elif (small < (goal % 5)):
     return -1
   elif ((big * 5) > goal):
-    dif = (big * 5) - goal
-    if ((dif % 5) != 0):
-      dif += 5 - (dif % 5)
-    big -= (dif / 5)
-    if ((small + (big * 5)) < goal):
-      return -1
-    else:
-      goal -= big * 5
-      return goal
+    dif = (big - (goal / 5))
+    big -= dif
+    goal -= (big * 5)
+    return goal
   else:
     goal -= big * 5
     return goal
