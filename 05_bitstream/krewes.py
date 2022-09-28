64818ad5d84e9060400c4810192cb175a62c752f
vgraeber30 <vgraeber30@stuy.edu>
2022-09-28 09:23:16 -0400

    Update krewes.py

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,18 @@
+"""
+Ziying Jian, Vivian Graeber
+SoftDev
+K05 -- bitstream
+2022-09-28
+time spent: <elapsed time in hours, rounded to nearest tenth>
+"""
+"""
+DISCO:
+
+QCC:
+
+OPS: 
+"""
+
+info = open("krewes.txt", "r")
+info.read()
+print(info.read())
