00e2c7bf33fc3e70675693629c4387c4ccaf4010
vgraeber30 <vgraeber30@stuy.edu>
2022-11-28 16:53:25 -0500

    made it better

--- 21_APIcard/app.py
+++ 21_APIcard/app.py
@@ -13,14 +13,30 @@ def question():
     data_json = json.loads(call.read())
     results_json = data_json["results"]
     difficulty = results_json[0]["difficulty"]
+    type = results_json[0]["type"]
     question = results_json[0]["question"]
     correct = results_json[0]["correct_answer"]
     incorrect = results_json[0]["incorrect_answers"]
     print(difficulty)
+    print(type)
     print(question)
     print(correct)
     print(incorrect)
-    return render_template("main.html", url=url, explanation = explanation)
+    #sorting all of the answers
+    if (type == "boolean"):
+        all_answers = ['True', 'False']
+        all_answers.sort(reverse=True)
+    elif (type == "multiple"):
+        all_answers = incorrect.copy()
+        all_answers.append(correct)
+        all_answers.sort()
+    #making the 2 types of quotes print correctly
+    question = question.replace("&quot;", '"')
+    question = question.replace("&#039;", "'")
+    for i in range(len(all_answers)):
+        all_answers[i] = all_answers[i].replace("&quot;", '"')
+        all_answers[i] = all_answers[i].replace("&#039;", "'")
+    return render_template("main.html", question = question, answers = all_answers)
 
 if __name__ == "__main__":
     app.debug = True
