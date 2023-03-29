c64c3f320a2c27b539722226740433ea3d814b88
vgraeber30 <vgraeber30@stuy.edu>
2023-03-29 09:42:13 -0400

    testing

--- tests/03_py/test_warmup_1.py
+++ tests/03_py/test_warmup_1.py
@@ -1,5 +1,5 @@
 def test_sleep_in(weekday, vacation):
-  assert = sleep_in(False, False) = True
-  assert = sleep_in(True, False) = False
-  assert = sleep_in(False, True) = True
-  assert = sleep_in(True, True) = True
+  assert sleep_in(False, False) == True
+  assert sleep_in(True, False) == False
+  assert sleep_in(False, True) == True
+  assert sleep_in(True, True) == True
